862386d42cc871ba31ce0baf796d3f55257183ac
hbj520 <hbjmailformac@163.com>
2016-09-28 20:39:03 +0800

    添加文件--han

---
+++ ConsumeTreasure/ConsumeTreasure/ViewControllers/Mine/MineTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MineTableViewController.h
+//  ConsumeTreasure
+//
+//  Created by youyou on 9/28/16.
+//  Copyright © 2016 youyou. All rights reserved.
+//  我的个人界面控制器
+
+#import <UIKit/UIKit.h>
+
+@interface MineTableViewController : UITableViewController
+
+@end
---
+++ ConsumeTreasure/ConsumeTreasure/ViewControllers/Union/UnionViewController.h
@@ -0,0 +1,13 @@
+//
+//  UnionViewController.h
+//  ConsumeTreasure
+//
+//  Created by youyou on 9/28/16.
+//  Copyright © 2016 youyou. All rights reserved.
+//  联盟控制器
+
+#import "BaseViewController.h"
+
+@interface UnionViewController : BaseViewController
+
+@end
