fc5cc4695d02751071bf132505197ee37af74ace
hbj520 <hbjmailformac@163.com>
2016-09-28 13:46:56 +0800

    添加Baseviewcontroller--han

---
+++ ConsumeTreasure/ConsumeTreasure/ViewControllers/Homepage/HomepageViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomepageViewController.h
+//  ConsumeTreasure
+//
+//  Created by youyou on 9/28/16.
+//  Copyright © 2016 youyou. All rights reserved.
+//  首页控制器
+
+#import "BaseViewController.h"
+
+@interface HomepageViewController : BaseViewController
+
+@end
