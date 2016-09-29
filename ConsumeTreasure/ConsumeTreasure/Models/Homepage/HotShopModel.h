2fa0b91308eb7247b0149afb821dc25f110247a7
hbj520 <hbjmailformac@163.com>
2016-09-29 09:30:31 +0800

    添加首页model--han

---
+++ ConsumeTreasure/ConsumeTreasure/Models/Homepage/HotShopModel.h
@@ -0,0 +1,13 @@
+//
+//  HotShopModel.h
+//  ConsumeTreasure
+//
+//  Created by youyou on 9/28/16.
+//  Copyright © 2016 youyou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HotShopModel : NSObject
+
+@end
