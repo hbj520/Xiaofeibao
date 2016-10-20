18799a814ab10b9394fc9da67fd4555b731769cd
hbj520 <hbjmailformac@163.com>
2016-10-20 09:16:55 +0800

    添加联盟VC--han

--- ConsumeTreasure/ConsumeTreasure/ViewControllers/Homepage/HomepageViewController.m
+++ ConsumeTreasure/ConsumeTreasure/ViewControllers/Homepage/HomepageViewController.m
@@ -6,8 +6,6 @@
 //  Copyright © 2016 youyou. All rights reserved.
 //
 
-#define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
-
 //百度
 #import <BaiduMapAPI_Utils/BMKUtilsComponent.h>
 #import <BaiduMapAPI_Search/BMKSearchComponent.h>
--- ConsumeTreasure/ConsumeTreasure/ViewControllers/Union/UnionViewController.m
+++ ConsumeTreasure/ConsumeTreasure/ViewControllers/Union/UnionViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "UnionViewController.h"
-
+#import "LianmenRightCollectionViewCell.h"
+#define CollectionViewReuseId @"collectionReuseId"
+#define TableViewReuseId @"unionReuseId"
 @interface UnionViewController ()
+<UITableViewDelegate,
+UITableViewDataSource,
+UICollectionViewDelegate,
+UICollectionViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *leftTableView;
+@property (weak, nonatomic) IBOutlet UICollectionView *rightCollectionView;
 
 @end
 
@@ -17,13 +25,72 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self configNavBar];
+    [self configTableViewAndCollectionView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+#pragma mark -PrivateMethod
+- (void)configNavBar{
+    NSString *navTitle = @"商家联盟";
+    // self.navigationController.navigationBar.barTintColor = RGBACOLOR(253, 87, 54, 1);
+    NSDictionary *attributeDict = [NSDictionary dictionaryWithObjectsAndKeys:[UIFont systemFontOfSize:18.0],NSFontAttributeName,[UIColor whiteColor],NSForegroundColorAttributeName, nil];
+    self.navigationController.navigationBar.titleTextAttributes = attributeDict;
+    self.navigationItem.title = navTitle;
+}
+- (void)configTableViewAndCollectionView{
+    //tableView
+    self.leftTableView.delegate = self;
+    self.leftTableView.dataSource = self;
+    [self.leftTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:TableViewReuseId];
+    self.leftTableView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"lianmenImageCell"]];
+    
+    //collectionview
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    flowLayout.sectionInset = UIEdgeInsetsMake(10, 25, 10, 25);
+    flowLayout.minimumLineSpacing = 0;
+    flowLayout.minimumInteritemSpacing = 0;
+    flowLayout.itemSize = CGSizeMake(85, 85);
+    [self.rightCollectionView setCollectionViewLayout:flowLayout];
+    [self.rightCollectionView registerNib:[UINib nibWithNibName:@"LianmenRightCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:CollectionViewReuseId];
+    self.rightCollectionView.delegate = self;
+    self.rightCollectionView.dataSource = self;
+    self.rightCollectionView.backgroundColor = [UIColor whiteColor];
+    
+}
+#pragma mark - UITableVewDelegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 47.5;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *reusedId = TableViewReuseId;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reusedId forIndexPath:indexPath];
+    if (nil == cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reusedId];
+    }
+    cell.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"lianmenImageCell"]];
+    cell.textLabel.text = @"滨湖新区";
+    cell.textLabel.textColor = [UIColor darkGrayColor];
+    return cell;
+}
+#pragma mark - UICollectionViewDelegate
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 20;
+}
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    LianmenRightCollectionViewCell *cell = [self.rightCollectionView dequeueReusableCellWithReuseIdentifier:CollectionViewReuseId forIndexPath:indexPath];
+    return cell;
+}
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+}
 /*
 #pragma mark - Navigation
 
