275748a76f6cc0a4b75d3e1306ab17a8ef5c4858
youyoumacmini3 <youyoumacmini3@youyoumacmini3demac-mini.local>
2016-10-20 17:40:39 +0800

    我的商户finish-wang

---
+++ ConsumeTreasure/ConsumeTreasure/ViewControllers/Homepage/ImUnionStoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImUnionStoreViewController.h
+//  ConsumeTreasure
+//
+//  Created by youyoumacmini3 on 16/10/20.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ImUnionStoreViewController : BaseViewController
+
+@end
