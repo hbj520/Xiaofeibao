1e65294095748267e97b0d6d930609e4b47945a0
hbj520 <hbjmailformac@163.com>
2016-12-02 16:19:12 +0800

    待评价修改--han

--- ConsumeTreasure/ConsumeTreasure/Views/Mine/EvaluateTableViewCell.h
+++ ConsumeTreasure/ConsumeTreasure/Views/Mine/EvaluateTableViewCell.h
@@ -7,8 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+@class EvaluateListModel;
 typedef void (^clickBtnBlock)();
 @interface EvaluateTableViewCell : UITableViewCell
 @property (copy,nonatomic) clickBtnBlock clickOneMoreBlock;
 @property (copy,nonatomic) clickBtnBlock clickEvaluateBlock;
+- (void)configWithData:(EvaluateListModel *)data;
 @end
--- ConsumeTreasure/ConsumeTreasure/Views/Mine/EvaluateTableViewCell.m
+++ ConsumeTreasure/ConsumeTreasure/Views/Mine/EvaluateTableViewCell.m
@@ -7,7 +7,18 @@
 //
 
 #import "EvaluateTableViewCell.h"
+#import "EvaluateListModel.h"
+@interface EvaluateTableViewCell ()
+@property (weak, nonatomic) IBOutlet UIImageView *shopIconImageview;
+@property (weak, nonatomic) IBOutlet UILabel *shopNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *orderStatusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *goodsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (weak, nonatomic) IBOutlet UIButton *preEvaluateBtn;
+@property (weak, nonatomic) IBOutlet UIButton *oneMoreBtn;
 
+@end
 @implementation EvaluateTableViewCell
 
 - (void)awakeFromNib {
@@ -31,5 +42,7 @@
         self.clickEvaluateBlock();
     }
 }
-
+- (void)configWithData:(EvaluateListModel *)data{
+    
+}
 @end
