61c6d2f1d36f24a898c09608db4ef61780d3a77b
hbj520 <hbjmailformac@163.com>
2016-12-29 20:50:46 +0800

    rewrite -- wang

---
+++ ConsumeTreasure/ConsumeTreasure/Views/Homepage/StoreConLabTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  StoreConLabTableViewCell.h
+//  ConsumeTreasure
+//
+//  Created by youyou on 16/12/29.
+//  Copyright © 2016年 youyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoreConLabTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *leftLab;
+@property (weak, nonatomic) IBOutlet UILabel *detailLab;
+
+
+@end
