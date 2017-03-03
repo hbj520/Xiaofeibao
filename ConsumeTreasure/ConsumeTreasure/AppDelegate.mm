e85baf5c2c1f01a329d77e0d4b122eba41d4669f
hbj520 <472237131@qq.com>
2017-03-03 14:06:10 +0800

    app delegate --wang

--- ConsumeTreasure/ConsumeTreasure/AppDelegate.mm
+++ ConsumeTreasure/ConsumeTreasure/AppDelegate.mm
@@ -15,7 +15,28 @@
 
 #import <AFNetworking.h>
 
-@interface AppDelegate ()
+// 极光推送
+#import "JPUSHService.h"
+#import <AdSupport/AdSupport.h>
+#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+#import <UserNotifications/UserNotifications.h> // 这里是iOS10需要用到的框架
+#endif
+
+
+static NSString * const JPUSHAPPKEY = @"d405d2a2e20ec28537f2d63a"; // 极光appKey
+static NSString * const channel = @"channel"; // 固定的
+
+#ifdef DEBUG // 开发
+
+static BOOL const isProduction = FALSE; // 极光FALSE为开发环境
+
+#else // 生产
+
+static BOOL const isProduction = TRUE; // 极光TRUE为生产环境
+
+#endif
+
+@interface AppDelegate ()<JPUSHRegisterDelegate>
 
 @end
 
@@ -40,9 +61,140 @@
     //友盟
     //设置友盟appkey
     [self configThirdLogin];
+    
+    
+    // 注册apns通知
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 10.0) // iOS10
+    {
+#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+        JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];
+        entity.types = UNAuthorizationOptionAlert|UNAuthorizationOptionBadge | UNAuthorizationOptionSound;
+        [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
+#endif
+    }
+    else if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) // iOS8, iOS9
+    {
+        //可以添加自定义categories
+        [JPUSHService registerForRemoteNotificationTypes:(UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert) categories:nil];
+    }
+    else // iOS7
+    {
+        //categories 必须为nil
+        [JPUSHService registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert) categories:nil];
+    }
+    /*
+     *  launchingOption 启动参数.
+     *  appKey 一个JPush 应用必须的,唯一的标识.
+     *  channel 发布渠道. 可选.
+     *  isProduction 是否生产环境. 如果为开发状态,设置为 NO; 如果为生产状态,应改为 YES.
+     *  advertisingIdentifier 广告标识符（IDFA） 如果不需要使用IDFA，传nil.
+     * 此接口必须在 App 启动时调用, 否则 JPush SDK 将无法正常工作.
+     */
+    // 广告标识符
+    NSString *advertisingId = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
+    
+    // 如不需要使用IDFA，advertisingIdentifier 可为nil
+    // 注册极光推送
+    [JPUSHService setupWithOption:launchOptions appKey:JPUSHAPPKEY channel:channel apsForProduction:isProduction advertisingIdentifier:advertisingId];
+    
+    //2.1.9版本新增获取registration id block接口。
+    [JPUSHService registrationIDCompletionHandler:^(int resCode, NSString *registrationID) {
+        if(resCode == 0)
+        {
+            // iOS10获取registrationID放到这里了, 可以存到缓存里, 用来标识用户单独发送推送
+            NSLog(@"registrationID获取成功：%@",registrationID);
+            [[NSUserDefaults standardUserDefaults] setObject:registrationID forKey:@"registrationID"];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+        }
+        else
+        {
+            NSLog(@"registrationID获取失败，code：%d",resCode);
+        }
+    }];
+
+    
     return YES;
    
 }
+
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    /// Required - 注册 DeviceToken
+    [JPUSHService registerDeviceToken:deviceToken];
+}
+
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    //Optional
+    NSLog(@"did Fail To Register For Remote Notifications With Error: %@", error);
+}
+
+//----------------------------------------------------------------------
+
+#pragma mark - iOS7: 收到推送消息调用
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    
+    // iOS7之后调用这个
+    [JPUSHService handleRemoteNotification:userInfo];
+    NSLog(@"iOS7及以上系统，收到通知");
+    
+    if ([[UIDevice currentDevice].systemVersion floatValue] < 10.0 || application.applicationState > 0)
+    {
+        // 程序在前台或通过点击推送进来的会弹这个alert
+        NSString *message = [NSString stringWithFormat:@"iOS7-8-9收到的推送%@", [userInfo[@"aps"] objectForKey:@"alert"]];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:message delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil, nil];
+        [alert show];
+    }
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+//----------------------------------------------------------------------
+#pragma mark - iOS10: 收到推送消息调用(iOS10是通过Delegate实现的回调)
+#pragma mark- JPUSHRegisterDelegate
+#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+// 当程序在前台时, 收到推送弹出的通知
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler {
+    
+    NSDictionary * userInfo = notification.request.content.userInfo;
+    
+    if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]])
+    {
+        [JPUSHService handleRemoteNotification:userInfo];
+        NSString *message = [NSString stringWithFormat:@"will%@", [userInfo[@"aps"] objectForKey:@"alert"]];
+        NSLog(@"iOS10程序在前台时收到的推送: %@", message);
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:message delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil, nil];
+        [alert show];
+    }
+    
+    completionHandler(UNNotificationPresentationOptionBadge|UNNotificationPresentationOptionSound|UNNotificationPresentationOptionAlert); // 需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以设置
+}
+
+// 程序关闭后, 通过点击推送弹出的通知
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler {
+    
+    NSDictionary * userInfo = response.notification.request.content.userInfo;
+    
+    if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]])
+    {
+        [JPUSHService handleRemoteNotification:userInfo];
+        NSString *message = [NSString stringWithFormat:@"did%@", [userInfo[@"aps"] objectForKey:@"alert"]];
+        NSLog(@"iOS10程序关闭后通过点击推送进入程序弹出的通知: %@", message);
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示" message:message delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil ,nil];
+        [alert show];
+    }
+    
+    completionHandler();  // 系统要求执行这个方法
+}
+#endif
+
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    
+    // Required,For systems with less than or equal to iOS6
+    [JPUSHService handleRemoteNotification:userInfo];
+}
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -52,10 +204,14 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    [application setApplicationIconBadgeNumber:0];
+    [JPUSHService setBadge:0];
+    [application cancelAllLocalNotifications];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
