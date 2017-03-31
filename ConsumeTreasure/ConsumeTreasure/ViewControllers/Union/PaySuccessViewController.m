f6c552b95de49d1df7af887bccb3f9fcd9ce5458
king <wq04200@163.com>
2017-03-31 17:59:27 +0800

    成功支付 -- 王

---
+++ ConsumeTreasure/ConsumeTreasure/ViewControllers/Union/PaySuccessViewController.m
@@ -0,0 +1,56 @@
+//
+//  PaySuccessViewController.m
+//  ConsumeTreasure
+//
+//  Created by youyoumacmini3 on 2017/3/31.
+//  Copyright © 2017年 youyou. All rights reserved.
+//
+
+#import "PaySuccessViewController.h"
+
+@interface PaySuccessViewController ()
+
+@end
+
+@implementation PaySuccessViewController
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBarHidden = NO;
+    self.tabBarController.tabBar.hidden = YES;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.navigationController setNavigationBarHidden:NO];
+    [self.navigationController.navigationBar setTitleTextAttributes:@{NSFontAttributeName:[UIFont boldSystemFontOfSize:16],NSForegroundColorAttributeName:[UIColor whiteColor]}];
+    self.automaticallyAdjustsScrollViewInsets = NO;
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)donePayClick:(id)sender {
+    NSLog(@"支付成功");
+    
+    
+}
+
+- (IBAction)back:(id)sender {
+    [self backTolastPage];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
